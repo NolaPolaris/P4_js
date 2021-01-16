426db805488b19923b7a94d6a0f5a2a817185c9b
Julie <julietaubry@gmail.com>
2021-01-16 10:47:23 +0100

    stylisation HTML

--- generateur.js
+++ generateur.js
@@ -1,10 +1,3 @@
-function show_quotes(){
-    // if div = empty
-    // ineerHTML.recueil_citation;
-    // else clean div && innerHTML.recueil_citation;
-
-}
-
 function Citation(debut, milieu, fin)  {
     this.debut = debut;
     this.milieu = milieu;
@@ -18,7 +11,8 @@ function Citation(debut, milieu, fin)  {
         var randomDebut = this.debut[this.getRandomInt(this.debut.length)];
         var randomMilieu = this.milieu[this.getRandomInt(this.milieu.length)];
         var randomFin = this.fin[this.getRandomInt(this.fin.length)];
-        return randomDebut + '\n' + randomMilieu + '\n' + randomFin;
+        return [randomDebut, randomMilieu, randomFin];
+        return `${randomDebut} \n ${randomMilieu} \n ${randomFin}`;
     }
     
 };
@@ -30,56 +24,47 @@ var haiku_data = {
         "En me baladant,",
         "En réfléchissant,",
         "En te dessinant,",
-        "En arrivant "
+        "En arrivant,"
     ],
     
     milieu : [
-        "entre les murs silencieux ",
-        "dans mon lit tout embrumé ",
-        "le long des chemins d'automne ",
-        "à la fin d'un jour trop gris ",
-        "quand l'aurore se dessine "
+        "entre les murs silencieux",
+        "dans mon lit tout embrumé",
+        "le long des chemins d'automne",
+        "à la fin d'un jour trop gris",
+        "quand l'aurore se dessine"
     ],
     
     fin : [
-        "j'arrive à rêver",
-        "un silence passe",
-        "mon chat me regarde",
-        "la lune souris",
-        "une étoile surgit"
+        "j'arrive à rêver.",
+        "un silence passe.",
+        "mon chat me regarde.",
+        "la lune souris.",
+        "une étoile surgit."
     ]
     
 }
 
-// var haiku = new Citation(haiku_data.debut, haiku_data.milieu, haiku_data.fin);
-// var haiku_generated = haiku.generate();
-// console.log(haiku_generated);
-
-
 var bullshit_data = {
     
     debut : ["Où que tu sois,",
-    "si tu te perds,",
+    "Si tu te perds,",
     "Si tu as des doutes,",
     "Tu es en retard,",
-    "Si tu fatigues "],
+    "Si tu fatigues, "],
     
-    milieu : ["appelle ta mère",
-    "commande un uber ",
-    "prends un chemin ",
-    "ouvre ta voie ",
-    "regarde devant toi "],
+    milieu : ["appelle ta mère,",
+    "commande un uber, ",
+    "prends un chemin, ",
+    "ouvre ta voie, ",
+    "regarde devant toi, "],
     
     fin : ["et reposes-toi.",
-    "c'est un bon choix",
-    "et abstiens-toi",
-    "et reviens chez moi",
-    "et ne t'arrêtes pas"]
+    "c'est un bon choix.",
+    "et abstiens-toi.",
+    "et reviens chez moi.",
+    "et ne t'arrêtes pas."]
 };
-
-// var bullshit = new Citation(bullshit_data.debut, bullshit_data.milieu, bullshit_data.fin);
-// var bullshit_generated = bullshit.generate();
-
 
 $(document).ready(function () {
     $("#send").click(function (event){
@@ -89,7 +74,7 @@ $(document).ready(function () {
         var qtt_citation = $("input#qtt");
         console.log(qtt_citation.val());
 
-        let recueil_citation = new Array;
+        
         let generator;
 
         if (theme_selected.val() == "haiku"){
@@ -102,22 +87,32 @@ $(document).ready(function () {
             window.alert("choisissez un thème !")
         }
 
-        let quote_generated;
-        for (i=0; i <qtt_citation.val(); i++){
-            quote_generated = generator.generate();
+        let recueil_citation = new Array;
+        
+        for (i=0; i < qtt_citation.val(); i++){
+            let quote_array = generator.generate();
+            let quote_generated = "";
+
+            quote_generated = "<div class='quote'>";
+            
+            quote_array.forEach(element => {
+                quote_generated += '<p>' + element + '</p>';
+            });
+            quote_generated += "</div>";
+
             recueil_citation.push(quote_generated);
         }
 
         console.log(recueil_citation);
-        // show_quotes(recueil_citation);
 
+        let content = document.getElementById('content');
+
+        content.innerHTML = recueil_citation.join(""); 
+        
     });
     
 
 });
 
-
-
-//   HTMLSelectElement.selectedIndex
 
 
